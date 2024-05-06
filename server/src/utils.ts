9322f489300c76605b46a940e512436d1013b64b
Krittin Chuaiphikroh <krittin@42bangkok.com>
2024-05-06 12:09:23 +0900

    [DNM] Fixup! Invalid requests
    Change-Id: I4e385fba8f34213bce7ed44fb957bf558d6e0610
    Signed-off-by: Krittin Chuaiphikroh <krittin@42bangkok.com>

--- server/src/utils.ts
+++ server/src/utils.ts
@@ -49,8 +49,10 @@ export const getIpFromRequest = function(req: express.Request): string | null {
 
 export const getHostNameFromRequest = async function(req: express.Request): Promise<string> {
 	let init_hostname = req.params.hostname ?? 'unknown';
-
-	let hostname = init_hostname.replace(/\.42bangkok\.com$/, '');
+	let hostname = init_hostname;
+	if (!init_hostname.includes(".42bangkok.com")) {
+		hostname = init_hostname + ".42bangkok.com";
+	}
 	// If hostname is not defined, parse it from the IP address
 	if (hostname === 'unknown') {
 		const ip = getIpFromRequest(req);
@@ -117,13 +119,13 @@ export const getMessageForHostName = async function(hostName: string): Promise<s
 			console.warn('Could not parse messages.json, unable to find messages for host');
 			return "";
 		}
-	
+
 		// Find messages for host
 		// Any message with a key that the hostname starts with will be returned
 		const hostMessages = Object.entries(messagesJson)
 			.filter(([key]) => hostName.startsWith(key))
 			.map(([, message]) => message);
-	
+
 		// Combine all messages into one
 		return hostMessages.join('\n\n');
 	} catch (error) {
